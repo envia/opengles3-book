653fac8d2bcc57b6f9823703ab22ea72ce042a8b
Hwang Joonhyung <envia@envia.pe.kr>
2026-01-02 15:13:43 +0900

    hello_triangle, part 2

--- envia/hello_triangle.c
+++ envia/hello_triangle.c
@@ -1,6 +1,21 @@
 #include <GLES3/gl3.h>
+#include <stdlib.h>
+
+GLboolean es_create_window()
+{
+	return GL_TRUE;
+}
+
+int es_main()
+{
+	es_create_window();
+	return GL_TRUE;
+}
 
 int main()
 {
-	return GL_TRUE;
+	if (es_main() != GL_TRUE) {
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
